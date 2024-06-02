7d782acf6755a99bea080f2f31ac0b07760e148a
mustafaguler3 <mustafa.glr@hotmail.com>
2024-06-02 12:56:38 +0300

    .

--- src/ExercisesThird.java
+++ src/ExercisesThird.java
@@ -1,9 +1,17 @@
 public class ExercisesThird {
     public static void main(String[] args) {
-
+        System.out.println(reverseString("mustafa"));
     }
 
 
+    static String reverseString(String str) {
+        String rev = "";
+        for (int i = str.length()-1;i>=0;i--){
+            rev += str.charAt(i);
+        }
+
+        return rev;
+    }
     static boolean isPalindrome(String str){
         int left = 0;
         int right = str.length() - 1;
